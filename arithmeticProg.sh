5e0c4a7b26fd7df921e77d42c0936fadb4cc8e50
Archana-Bhamare <archusonar2@gmail.com>
2020-10-09 19:02:27 +0530

    Added : Compute a+b*c

--- arithmeticProg.sh
+++ arithmeticProg.sh
@@ -1,5 +1,8 @@
-#!/bin/bash 
+#!/bin/bash -x
 
 read -p "Enter the value of a :" a
 read -p "Enter the value of b :" b
 read -p "Enter the value of c :" c
+
+result=$((a+b*c))
+echo "Result : $result"

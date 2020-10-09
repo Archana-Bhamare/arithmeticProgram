d1254a782d017b7eacc751f7255d1d5c2b3648f4
Archana-Bhamare <archusonar2@gmail.com>
2020-10-09 19:49:22 +0530

    Added : Store results in Dictionary

--- arithmeticProg.sh
+++ arithmeticProg.sh
@@ -5,14 +5,21 @@ read -p "Enter the value of a : " a
 read -p "Enter the value of b : " b
 read -p "Enter the value of c : " c
 
-result=$((a+b*c))
-echo "Result : $result"
+result1=$((a+b*c))
+echo "Result of a+b*c : $result1"
 
-result=$((a*b+c))
-echo "Result : $result"
+result2=$((a*b+c))
+echo "Result of a*b+c : $result2"
 
-result=$((c+a/b))
-echo "Result : $result"
+result3=$((c+a/b))
+echo "Result of c+a/b : $result3"
 
-result=$((a%b+c))
-echo "Result : $result"
+result4=$((a%b+c))
+echo "Result of a%b+c: $result4"
+
+declare -A arithDict
+
+arithDict[one]="$result1"
+arithDict[two]="$result2"
+arithDict[three]="$result3"
+arithDict[four]="$result4"
